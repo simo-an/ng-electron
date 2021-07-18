2d276dc0fe71017840ddd536341c9d3bbcf17637
simumn <seimor@126.com>
2021-07-18 14:57:03 +0800

    Feat: add the simple ipc communication. PING -> PONG

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import * as path from 'path';
 
 let win: BrowserWindow = null;
@@ -21,47 +21,32 @@ function createWindow(): BrowserWindow {
   const winURL = isDevelopment
     ? 'http://localhost:4200'
     : `file://${path.join(__dirname, '/../renderer/index.html')}`;
+
   if (isDevelopment) {win.webContents.openDevTools();}
 
-  win.loadURL(winURL).then();
+  win.loadURL(winURL).then(() => {});
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
+  win.on('closed', () => { win = null; });
 
   return win;
 }
 
 try {
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  // Added 400 ms to fix the black background issue while using transparent window.
-  // More details at https://github.com/electron/electron/issues/15947
   app.on('ready', () => setTimeout(createWindow, 400));
 
   // Quit when all windows are closed.
   app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
+    if (process.platform !== 'darwin') { app.quit(); }
   });
 
   app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow();
-    }
+    if (win === null) { createWindow(); }
   });
 
+  // handle the ping from renderer
+  ipcMain.handle('PING', () => 'PONG');
+
 } catch (e) {
-  // Catch Error
-  // throw e;
+  console.warn('Err: ', e);
+  process.exit(0);
 }
--- src/renderer/app/app.component.ts
+++ src/renderer/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ipcRenderer } from 'electron';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgElectron';
+
+  constructor() {
+    ipcRenderer.invoke('PING').then((result) => {
+      console.warn('Renderer Receive: ', result);
+    });
+  }
 }
